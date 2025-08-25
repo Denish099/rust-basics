992dcb1642a0ad195d720d492978901971e61ec4
Denish099 <gyldenish@gmail.com>
2025-08-25 17:20:42 +0530

    added numbers in project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let mut x: i8 = 2;  // make x mutable and use bigger type
+    for i in 1..1000 {
+        x = x + 100;
+    }
+    println!("Final x = {}", x);
 }
+
+// cargo init to intialize a rust project
+// cargo run to run it 
