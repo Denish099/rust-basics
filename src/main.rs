bab4173a6b9564fcb6492969bf0a8419a4666b42
Denish099 <gyldenish@gmail.com>
2025-08-25 23:38:55 +0530

    added string

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
+
 fn main() {
-    let mut x: i8 = 2;  // make x mutable and use bigger type
-    for i in 1..1000 {
-        x = x + 100;
-    }
-    println!("Final x = {}", x);
+    // let mut x: i8 = 2;  // make x mutable and use bigger type
+    // for i in 1..1000 {
+    //     x = x + 100;
+    // }
+    // println!("Final x = {}", x);
+
+    let greeting :String = String::from("hello world");
+
+    println!("{}",greeting);
 }
 
 // cargo init to intialize a rust project
 // cargo run to run it 
+
+
+// string in rust does not have fixed type
+// string can change spadce in runtime 
