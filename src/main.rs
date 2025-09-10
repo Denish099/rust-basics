3813b096733199b1884679dc7515c5a48a12118d
Denish099 <gyldenish@gmail.com>
2025-09-10 11:12:55 +0530

    strings ownership

--- src/main.rs
+++ src/main.rs
@@ -36,14 +36,35 @@
 // irresplective of string number all data types in rust are immutable
 // to make it mutable use mut after let 
 
-fn main(){
-    let x = 1;  //created on stack , owner is main function
-    let y = 2;  //created on stack owner is main function
-    println!("{}",sum(x,y));
-    println!("hello world");
-}
+// fn main(){
+//     let x = 1;  //created on stack , owner is main function
+//     let y = 2;  //created on stack owner is main function
+//     println!("{}",sum(x,y));
+//     println!("hello world");
+// }
 
-fn sum(a:i32,b:i32)->i32{
-    let c = a+b;  //created a,b,c on stack owner is sum function
-    return c;
-}
+// fn sum(a:i32,b:i32)->i32{
+//     let c = a+b;  //created a,b,c on stack owner is sum function
+//     return c;
+// }
+
+
+// fn main(){
+//     let s1 = String::from("hi there");
+//     print!("{}",s1);
+//     let s2 = s1;
+//     print!("{}",s2);
+// }
+
+//if the owner goes out of scope and the coressponding vaiabkealso goes out of scope so this is why rust is so safe
+
+// fn main(){
+//     let s1= String::from("hello");
+//     takeownership(s1.clone());
+
+//     print!("{}",s1);
+// }
+
+// fn takeownership(some_string:String){
+//     print!("{}",some_string);
+// }
